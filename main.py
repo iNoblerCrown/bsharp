48be5546ad5a5e76f557edf3450120790f3aea2a
CodeMarketplace <mail.codemarketplace@gmail.com>
2023-06-24 21:26:45 +0200

    first main

---
+++ main.py
@@ -0,0 +1,30 @@
+reservedWords = []
+testList = ["aa:aaa", "a", "a"]
+def lineSeperator(inputstring):
+    l = inputstring.split("\n")
+    return l
+
+def getReservedWordID(inputList):
+    IDs = {}
+    for x in range(len(inputList)):
+        d = {
+            x : ""
+        }
+        IDs.update(d)
+    l = []
+    t = []
+    for _ in range(len(inputList)):
+        l.append("")
+        for y in range(len(inputList[_])):
+            if inputList[_][y] != ":":
+                l[_] = l[_] + inputList[_][y]
+                t.append(-1)
+            else:
+                t[_] = y
+                break
+    
+
+
+
+
+print(getReservedWordID(testList))
